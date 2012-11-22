5ceb65edd98d0ccb4582f7d97bd6eeadbc537ebb
Gav Parkes (Work) <gavin.parkes@KGBHUW-208.EMEA.FS.UTC.com>
2012-11-22 17:29:47 +0000

    general

---
+++ src/uk/co/parkesfamily/parkes/pissup/guide/WineListActivity.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package uk.co.parkesfamily.parkes.pissup.guide;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * @author gavin.parkes
+ *
+ */
+public class WineListActivity extends FragmentActivity {
+
+}
